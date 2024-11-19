e3f03b83093db2dd8c5793866b3757a9a606d94a
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-11-19 21:42:01 +0530

    Create 2_Search_node_BST.py

---
+++ 2_Search_node_BST.py
@@ -0,0 +1,58 @@
+from pickle import PROTO
+
+
+class BST:
+
+    def __init__(self,data):
+        self.lchild = None
+        self.rchild = None
+        self.data = data
+
+    def add_node(self,n):
+        #if Root is Empty
+        if self.data is None:
+            self.data = n
+            return
+        #if Duplicated
+        if self.data == n:
+            return
+        #if given n is less than the Root node
+        if self.data > n:
+            if self.lchild:
+                self.lchild.add_node(n)
+            else:
+                self.lchild = BST(n)
+
+        else:
+            if self.rchild:
+                self.rchild.add_node(n)
+            else:
+                self.rchild = BST(n)
+
+
+    def search(self,n,c=0):
+        if self.data == n:
+            print("Node Found...! at position {}".format(c))
+            return
+        if self.data > n:
+            if self.lchild:
+                self.lchild.search(n,c+1)
+            else:
+                print("Node Not Found....!")
+        else:
+            if self.data > n:
+                self.lchild.search(n,c+1)
+            else:
+                print("Node Not Found....!")
+
+    
+
+root = BST(10)
+l = [20,4,30,4,1,5,6]
+for i in l:
+    root.add_node(i)
+print("Root Node:",root.data)
+print("Left Child:",root.lchild.data)
+print("Right Child:",root.rchild.data)
+
+root.search(1)
