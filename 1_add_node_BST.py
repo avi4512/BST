87f3d9b8b42b29879520dc143ca430bde6287412
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-11-19 19:31:54 +0530

    Update 1_add_node_BST.py

--- 1_add_node_BST.py
+++ 1_add_node_BST.py
@@ -1,3 +1,6 @@
+from pickle import PROTO
+
+
 class BST:
 
     def __init__(self,data):
@@ -29,3 +32,6 @@ root = BST(10)
 l = [20,4,30,4,1,5,6]
 for i in l:
     root.add_node(i)
+print("Root Node:",root.data)
+print("Left Child:",root.lchild.data)
+print("Right Child:",root.rchild.data)
