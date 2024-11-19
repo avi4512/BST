3960811a32c238ad1059a1f908c60c5362a0b23d
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-11-19 19:24:17 +0530

    Create 1_add_node.py

---
+++ 1_add_node.py
@@ -0,0 +1,31 @@
+class BST:
+
+    def __init__(self,data):
+        self.lchild = None
+        self.rchild = None
+        self.data = data
+
+    def add_node(self,n):
+        #if Root is Empty
+        if self.data is None:
+            self.data = n
+            return
+        #if Duplicated
+        if self.data == n:
+            return
+        #if given n is less than the Root node
+        if self.data > n:
+            if self.lchild:
+                self.lchild.add_node(n)
+            else:
+                self.lchild = BST(n)
+
+        else:
+            if self.rchild:
+                self.rchild.add_node(n)
+            else:
+                self.rchild = BST(n)
+root = BST(10)
+l = [20,4,30,4,1,5,6]
+for i in l:
+    root.add_node(i)
