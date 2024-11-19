cfb42a40b7427c40e8d628952667aa1e5dada0a3
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-11-19 21:44:31 +0530

    Create 3_Pre_Order_BST.py

---
+++ 3_Pre_Order_BST.py
@@ -0,0 +1,68 @@
+from pickle import PROTO
+
+
+class BST:
+
+    def __init__(self,data):
+        self.lchild = None
+        self.rchild = None
+        self.data = data
+
+    def add_node(self,n):
+        #if Root is Empty
+        if self.data is None:
+            self.data = n
+            return
+        #if Duplicated
+        if self.data == n:
+            return
+        #if given n is less than the Root node
+        if self.data > n:
+            if self.lchild:
+                self.lchild.add_node(n)
+            else:
+                self.lchild = BST(n)
+
+        else:
+            if self.rchild:
+                self.rchild.add_node(n)
+            else:
+                self.rchild = BST(n)
+
+
+    def search(self,n,c=0):
+        if self.data == n:
+            print("Node Found...! at position {}".format(c))
+            return
+        if self.data > n:
+            if self.lchild:
+                self.lchild.search(n,c+1)
+            else:
+                print("Node Not Found....!")
+        else:
+            if self.data > n:
+                self.lchild.search(n,c+1)
+            else:
+                print("Node Not Found....!")
+
+    def pre_order(self):
+        print(self.data,' ',end=" ")
+        if self.lchild:
+            self.lchild.pre_order()
+        if self.rchild:
+            self.rchild.pre_order()
+            
+    
+
+
+
+root = BST(21)
+l = [10,30,5,12,25,100,3,7]
+for i in l:
+    root.add_node(i)
+print("Root Node:",root.data)
+print("Left Child:",root.lchild.data)
+print("Right Child:",root.rchild.data)
+
+root.search(1)
+root.pre_order()
